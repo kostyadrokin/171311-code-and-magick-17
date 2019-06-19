04a89171ab72fd4af496e71ae259b110eca9ce4f
Kostya <kostya@drokin.ru>
2019-06-20 00:35:07 +0300

    Сделал всё, кроме отправки формы

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,80 @@
 'use strict';
-document.querySelector('.setup').classList.remove('hidden');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setupWindow = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupWindow.querySelector('.setup-close');
+var userIcon = document.querySelector('.setup-open-icon');
+var buttonSubmit = setupWindow.querySelector('.setup-submit');
+var wizardCoatColor = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyesColor = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireballColor = document.querySelector('.setup-fireball-wrap');
+var userNameInput = document.querySelector('.setup-user-name');
+
+
+// Функция закрытия модального окна по нажатию на ESC
+var onPopupEscPress = function (evt) {
+  if (userNameInput != document.activeElement) { // Проверка фокуса в поле имени игрока
+    if (evt.keyCode === ESC_KEYCODE) {
+      closePopup();
+    }
+  }
+};
+
+// Функция открытия модального окна с разными обработчиками для объектов этого окна
+var openPopup = function () {
+  setupWindow.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+//  document.getElementsByClassName('setup-wizard-form').addEventListener('click', function () {
+ //   form.submit();
+ // });
+  buttonSubmit.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+        // Тут должна отправляться форма
+    }
+  });
+  wizardCoatColor.addEventListener('click', function () {
+    wizardCoatColor.style.fill = WIZARDS_COATCOLOR[getRandom(0, WIZARDS_COATCOLOR.length)];
+  });
+  wizardEyesColor.addEventListener('click', function () {
+    wizardEyesColor.style.fill = WIZARDS_EYESCOLOR[getRandom(0, WIZARDS_EYESCOLOR.length)];
+  });
+  wizardFireballColor.addEventListener('click', function () {
+    var fireballColor = WIZARDS_FIREBALLCOLOR[getRandom(0, WIZARDS_FIREBALLCOLOR.length)];
+    document.querySelector('.setup-fireball-wrap').style.background = fireballColor;
+    document.querySelector('.setup-fireball-input').value = fireballColor; ;
+  });
+};
+
+var closePopup = function () {
+  setupWindow.classList.add('hidden');
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+userIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// Валидация инпута имени игрока
+document.querySelector('.setup-user-name').maxlength = '25';
+document.querySelector('.setup-user-name').setAttribute('minlength', 2);
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 // Генерация случайного числа для выбора случайного имени и фамилии
@@ -45,6 +120,14 @@ var WIZARDS_EYESCOLOR = [
   'green'
 ];
 
+var WIZARDS_FIREBALLCOLOR = [
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848',
+  '#ee4830'
+];
+
 var wizards = [
   {
     name: WIZARDS_NAMES[getRandom(0, WIZARDS_NAMES.length)] + ' ' + WIZARDS_LASTNAMES[getRandom(0, WIZARDS_LASTNAMES.length)],
@@ -80,3 +163,5 @@ for (var i = 0; i < wizards.length; i++) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
 }
+
+
