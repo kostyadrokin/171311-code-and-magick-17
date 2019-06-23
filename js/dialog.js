50573d075d6ee054658c8fddb52e0f86ca656f1c
Kostya <kostya@drokin.ru>
2019-06-23 21:44:45 +0300

    Правки Трэвиса

--- js/dialog.js
+++ js/dialog.js
@@ -3,7 +3,8 @@
 var setupWindow = document.querySelector('.setup');
 var dialogHandler = setupWindow.querySelector('.upload');
 var itemsShopList = setupWindow.querySelector('.setup-artifacts-shop');
-var itemOnShopList = itemsShopList.querySelector('.setup-artifacts-cell');
+var itemOnShopList = itemsShopList.querySelector('.setup-artifacts-cell').querySelectorAll('img');
+var itemsBagsList = setupWindow.querySelector('.setup-artifacts').querySelectorAll('.setup-artifacts-cell');
 
 dialogHandler.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
@@ -52,10 +53,9 @@ dialogHandler.addEventListener('mousedown', function (evt) {
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 });
-/*
-console.log(itemsShopList);
 
-itemOnShopList.addEventListener('mousedown', function (evt) {
+
+itemOnShopList[0].addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
   var startCoordsItem = {
@@ -63,15 +63,48 @@ itemOnShopList.addEventListener('mousedown', function (evt) {
     y: evt.clientY
   };
 
-  var onMouseMove = function () {
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoordsItem.x - moveEvt.clientX,
+      y: startCoordsItem.y - moveEvt.clientY
+    };
+
+    startCoordsItem = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    itemOnShopList[0].style.position = 'absolute';
+    itemOnShopList[0].style.top = (itemOnShopList[0].offsetTop - shift.y) + 'px';
+    itemOnShopList[0].style.left = (itemOnShopList[0].offsetLeft - shift.x) + 'px';
+    itemOnShopList[0].style.zIndex = 1000;
 
   };
 
-  var onMouseUp = function () {
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    var sverkaCoordsStar = itemOnShopList[0].getBoundingClientRect();
+    for (var i = 0; i < itemsBagsList.length; i++) {
+      var sverkaCoordsBag = itemsBagsList[i].getBoundingClientRect();
 
+      if (sverkaCoordsStar.y + (sverkaCoordsStar.height / 2) >= sverkaCoordsBag.y &&
+        sverkaCoordsStar.y + (sverkaCoordsStar.height / 2) <= sverkaCoordsBag.y + 65 &&
+        sverkaCoordsStar.x + (sverkaCoordsStar.width / 2) >= sverkaCoordsBag.x &&
+        sverkaCoordsStar.x + (sverkaCoordsStar.width / 2) <= sverkaCoordsBag.x + 65
+          ) {
+            itemsBagsList[i].appendChild(itemOnShopList[0]);
+            itemOnShopList[0].style.top = 0 + 'px';
+            itemOnShopList[0].style.left = 0 + 'px';
+            } else {
+              itemOnShopList[0].removeAttribute('style');
+            }
+    }
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
   };
 
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 })
-*/
